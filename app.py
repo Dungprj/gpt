54e2757e02be1d1bff1c77f5c6f69d92ac0265ad
Dungpj <doibatcong2003@gmail.com>
2023-07-24 20:47:50 +0700

    vs1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("GPT Dũng pro vip")
 #cai dat model davanci 
 model = "text-davinci-003"
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 def get_response_from_chatgpt(user_question):
